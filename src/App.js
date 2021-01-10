79f44031d967cbc8216fe2a0a1ba9ad3cbb0742e
AgentTripleC <AgentTripleCCC@Gmail.com>
2021-01-09 16:55:18 -0800

    First commit
    Attempting all current products and converting towards google maps tools

--- src/App.js
+++ src/App.js
@@ -1,35 +1,39 @@
 import React, { Component } from 'react';
 import './App.css';
-import Map from './Map';
+import {
+  GoogleMap,
+  useLoadScript,
+  Marker,
+  InfoWindow
+} from "@react-google-maps/api";
 
-class App extends Component {
-  state = {
-    loading: true
-  };
-  componentDidMount () {
-    demoAsyncCall().then(() => this.setState({ loading: false }));
-  }
-  /*
-  constructor(){
-    super();
-  }*/
-  
-  render() {
-    const { loading } = this.state;
-    if(loading) {
-      return null;
-    }
-    return (
-      <div>
-        <div id="app" class="loader"></div> 
-        <Map />
-      </div>
-    )
-  }
+//avoids too many rerenders
+const libraries = ["places"];
+const mapContainerStyle = {
+  width: window.innerWidth,
+  height: window.innerHeight,
 }
 
-function demoAsyncCall() {
-  return new Promise((resolve) => setTimeout(() => resolve(), 200));
+const center = {
+  lat: 39.52766,
+  lng: -119.81353
 }
 
-export default App;
+export default function App() {
+  const {isLoaded, loadError} = useLoadScript({
+    googleMapsApiKey: process.env.REACT_APP_GOOGLE_MAPS_API_KEY,
+    libraries,
+  })
+
+  if(loadError) return "error loading maps";
+  if(!isLoaded) return "loading";
+
+  return <div>
+    <GoogleMap 
+    mapContainerStyle = {mapContainerStyle}
+    zoom = {10}
+    center = {center}>
+
+    </GoogleMap>
+  </div>
+}
